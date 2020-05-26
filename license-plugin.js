6e6ca48c0f0597ed076d5b6b24fdb6652b92a1a2
JesseVergelijkdirect <jesse@vergelijkdirect.com>
2020-05-26 14:20:03 +0200

    update master

--- license-plugin.js
+++ license-plugin.js
@@ -1,9 +1,24 @@
 import License from './components/license.vue'
 
-const MyPlugin = {
-  install(Vue, options = {}) {
-    Vue.component(License.name, License);
-  }
-};
+function install(Vue) {
+  if (install.installed) return
+  install.installed = true
+  Vue.component(License.name, License);
+}
 
-export default MyPlugin; 
+const plugin = {
+  install
+}
+
+let GlobalVue = null
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.vue
+}
+
+if (GlobalVue) {
+  GlobalVue.use(plugin)
+}
+
+component.install = install
